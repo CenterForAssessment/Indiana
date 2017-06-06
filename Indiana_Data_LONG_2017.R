4d67d5266c96d77f1682acc08852d9424e3d4911
dbetebenner <dbetebenner@gmail.com>
2017-06-06 16:59:21 -0400

    Update with 2017 scripts

---
+++ Indiana_Data_LONG_2017.R
@@ -0,0 +1,40 @@
+###########################################################################################
+###
+### Script for creating Indiana LONG data set for 2017
+###
+###########################################################################################
+
+### Load SGP Package
+
+require(data.table)
+
+
+### Load base data files
+
+Indiana_Data_LONG_2017 <- fread("Data/Base_Files/ISTEP_2017_Damian_Export_20171020.csv", colClasses=rep("character", 7))
+
+
+### Prepare Data
+
+setnames(Indiana_Data_LONG_2017, c("IDOE_CORPORATION_ID", "IDOE_SCHOOL_ID", "STN", "STUDENT_ID", "GRADE_ID", "ELA_SCALE", "MATH_SCALE"))
+Indiana_Data_LONG_2017[,"STN":=NULL]
+Indiana_Data_LONG_2017 <- rbindlist(list(Indiana_Data_LONG_2017[,c(1:5), with=FALSE], Indiana_Data_LONG_2017[,c(1:4,6), with=FALSE]))
+setnames(Indiana_Data_LONG_2017, "ELA_SCALE", "SCALE_SCORE")
+
+Indiana_Data_LONG_2017[,CONTENT_AREA:=rep(c("ELA", "MATHEMATICS"), each=dim(Indiana_Data_LONG_2017)[1]/2)]
+Indiana_Data_LONG_2017[,VALID_CASE:="VALID_CASE"]
+Indiana_Data_LONG_2017[,SCHOOL_YEAR:="2017"]
+
+Indiana_Data_LONG_2017[,GRADE_ID:=as.character(as.numeric(GRADE_ID))]
+Indiana_Data_LONG_2017[,SCALE_SCORE:=as.numeric(SCALE_SCORE)]
+
+
+### Take highest score for duplicates
+
+setkey(Indiana_Data_LONG_2017, VALID_CASE, SCHOOL_YEAR, CONTENT_AREA, STUDENT_ID, SCALE_SCORE)
+setkey(Indiana_Data_LONG_2017, VALID_CASE, SCHOOL_YEAR, CONTENT_AREA, STUDENT_ID)
+Indiana_Data_LONG_2017[which(duplicated(Indiana_Data_LONG_2017, by=key(Indiana_Data_LONG_2017)))-1, VALID_CASE:="INVALID_CASE"]
+
+### Save results
+
+save(Indiana_Data_LONG_2017, file="Data/Indiana_Data_LONG_2017.Rdata")
