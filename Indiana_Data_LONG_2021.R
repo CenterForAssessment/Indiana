71d79b13a0e5aa140455a3db4c11ec6e5ef453be
Damian Betebenner <dbetebenner@nciea.org>
2021-06-25 21:48:27 -0400

    Adding in 2021 data prep script

---
+++ Indiana_Data_LONG_2021.R
@@ -0,0 +1,42 @@
+###########################################################################################
+###
+### Script for creating Indiana LONG data set for 2021
+###
+###########################################################################################
+
+### Load SGP Package:
+
+require(data.table)
+
+
+### Load base data files
+
+Indiana_Data_LONG_2021 <- fread("Data/Base_Files/ILEARN_2021_Damian_Export_20210625.csv", colClasses=rep("character", 7))
+
+
+### Prepare Data
+
+setnames(Indiana_Data_LONG_2021, c("IDOE_CORPORATION_ID", "IDOE_SCHOOL_ID", "STN", "STUDENT_ID", "GRADE_ID", "ELA_SCALE", "MATH_SCALE"))
+Indiana_Data_LONG_2021[,"STN":=NULL]
+Indiana_Data_LONG_2021 <- rbindlist(list(Indiana_Data_LONG_2021[,c(1:5), with=FALSE], Indiana_Data_LONG_2021[,c(1:4,6), with=FALSE]), use.names=FALSE)
+setnames(Indiana_Data_LONG_2021, "ELA_SCALE", "SCALE_SCORE")
+
+Indiana_Data_LONG_2021[,CONTENT_AREA:=rep(c("ELA", "MATHEMATICS"), each=dim(Indiana_Data_LONG_2021)[1]/2)]
+Indiana_Data_LONG_2021[,VALID_CASE:="VALID_CASE"]
+Indiana_Data_LONG_2021[,SCHOOL_YEAR:="2021"]
+Indiana_Data_LONG_2021[,SCALE_SCORE:=as.numeric(SCALE_SCORE)]
+
+### INVALIDATE cases with missing SCALE_SCORE
+
+Indiana_Data_LONG_2021[is.na(SCALE_SCORE), VALID_CASE:="INVALID_CASE"]
+
+### Take highest score for duplicates
+
+setkey(Indiana_Data_LONG_2021, VALID_CASE, SCHOOL_YEAR, CONTENT_AREA, STUDENT_ID, SCALE_SCORE)
+setkey(Indiana_Data_LONG_2021, VALID_CASE, SCHOOL_YEAR, CONTENT_AREA, STUDENT_ID)
+Indiana_Data_LONG_2021[which(duplicated(Indiana_Data_LONG_2021, by=key(Indiana_Data_LONG_2021)))-1, VALID_CASE:="INVALID_CASE"]
+
+
+### Save results
+
+save(Indiana_Data_LONG_2021, file="Data/Indiana_Data_LONG_2021.Rdata")
