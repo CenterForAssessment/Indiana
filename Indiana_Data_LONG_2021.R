8b404808f499c04b049397caca99b0ebd998360c
Damian Betebenner <dbetebenner@nciea.org>
2021-06-28 22:23:10 -0400

    Adding in demographics merging into 2021 LONG data preparation

--- Indiana_Data_LONG_2021.R
+++ Indiana_Data_LONG_2021.R
@@ -12,6 +12,7 @@ require(data.table)
 ### Load base data files
 
 Indiana_Data_LONG_2021 <- fread("Data/Base_Files/ILEARN_2021_Damian_Export_20210625.csv", colClasses=rep("character", 7))
+Indiana_Demographics_2021 <- fread("Data/Base_Files/ILEARN_2021_demographics.csv", colClasses=rep("character", 5))
 
 
 ### Prepare Data
@@ -36,6 +37,16 @@ setkey(Indiana_Data_LONG_2021, VALID_CASE, SCHOOL_YEAR, CONTENT_AREA, STUDENT_ID
 setkey(Indiana_Data_LONG_2021, VALID_CASE, SCHOOL_YEAR, CONTENT_AREA, STUDENT_ID)
 Indiana_Data_LONG_2021[which(duplicated(Indiana_Data_LONG_2021, by=key(Indiana_Data_LONG_2021)))-1, VALID_CASE:="INVALID_CASE"]
 
+### Prepare Indiana_Demographics_2021
+
+setnames(Indiana_Demographics_2021, c("STUDENT_ID", "ETHNICITY", "SPECIAL_EDUCATION_STATUS", "SOCIO_ECONOMIC_STATUS", "ENGLISH_LANGUAGE_LEARNER_STATUS"))
+Indiana_Demographics_2021[,SCHOOL_YEAR:="2021"][,VALID_CASE:="VALID_CASE"]
+setkey(Indiana_Demographics_2021, VALID_CASE, SCHOOL_YEAR, STUDENT_ID)
+setkey(Indiana_Data_LONG_2021, VALID_CASE, SCHOOL_YEAR, STUDENT_ID)
+
+Indiana_Data_LONG_2021 <- Indiana_Demographics_2021[Indiana_Data_LONG_2021]
+
+setcolorder(Indiana_Data_LONG_2021, c(6,12,7,10,1,11,8,9,2,3,4,5))
 
 ### Save results
 
