1f846abe6e835ec13f16208e3db30c28169ebb74
Damian Betebenner <dbetebenner@nciea.org>
2022-07-05 22:14:29 -0400

    Updating 2022 PART B

--- Indiana_SGP_2022_PART_B.R
+++ Indiana_SGP_2022_PART_B.R
@@ -1,33 +1,49 @@
-####################################################################
-###
-### Code to update SGP analyses for Indiana for 2022
-###
-####################################################################
+################################################################################
+###                                                                          ###
+###                Indiana SGP analyses for 2022                             ###
+###                                                                          ###
+################################################################################
 
-### Load SGP Package
+###   Load packages
 require(SGP)
+require(SGPmatrices)
 
-
-### Load data
-load("Data/Indiana_Data_LONG_2022.Rdata")
+###   Load data
 load("Data/Indiana_SGP.Rdata")
+load("Data/Indiana_Data_LONG_2022.Rdata")
 
+###   Add Baseline matrices to SGPstateData
+SGPstateData <- addBaselineMatrices("IN", "2022")
+SGPstateData[["IN"]][["Assessment_Program_Information"]][["Assessment_Transition"]] <- NULL
 
-### updateSGP
+###   Read in SGP Configuration Scripts and Combine
+source("SGP_CONFIG/2022/PART_B/ELA.R")
+source("SGP_CONFIG/2022/PART_B/MATHEMATICS.R")
+
+IN_CONFIG <- c(ELA_2022.config, MATHEMATICS_2022.config)
+
+### Parameters
+parallel.config <- list(BACKEND="PARALLEL", WORKERS=list(PERCENTILES=4, BASELINE_PERCENTILES=4, PROJECTIONS=4, LAGGED_PROJECTIONS=4, SGP_SCALE_SCORE_TARGETS=4))
+
+#####
+###   Run updateSGP analysis
+#####
+
 Indiana_SGP <- updateSGP(
-			Indiana_SGP,
-			Indiana_Data_LONG_2022,
-			steps=c("prepareSGP", "analyzeSGP", "combineSGP", "outputSGP"),
-			sgp.percentiles=TRUE,
-			sgp.projections=TRUE,
-			sgp.projections.lagged=TRUE,
-			sgp.percentiles.baseline=TRUE,
-			sgp.projections.baseline=TRUE,
-			sgp.projections.lagged.baseline=TRUE,
-			save.intermediate.results=FALSE,
-			sgp.target.scale.scores=FALSE,
-			parallel.config=list(BACKEND="PARALLEL", WORKERS=list(TAUS=22)))
+        what_sgp_object = Indiana_SGP,
+        with_sgp_data_LONG = Indiana_Data_LONG_2022,
+        steps = c("prepareSGP", "analyzeSGP", "combineSGP"),
+        sgp.config = IN_CONFIG,
+        sgp.percentiles = TRUE,
+        sgp.projections = TRUE,
+        sgp.projections.lagged = TRUE,
+        sgp.percentiles.baseline = TRUE,
+        sgp.projections.baseline = TRUE,
+        sgp.projections.lagged.baseline = TRUE,
+        save.intermediate.results = FALSE,
+        parallel.config = parallel.config
+)
 
 
-### Save results
+###   Save results
 #save(Indiana_SGP, file="Data/Indiana_SGP.Rdata")
