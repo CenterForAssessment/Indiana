22c8906568e99977cdf0de16f37e44eecdcf04e3
Damian Betebenner <dbetebenner@nciea.org>
2025-05-05 09:28:36 -0400

    Update of scripts

--- sgpFlow/Indiana_sgpFlow_2024.R
+++ sgpFlow/Indiana_sgpFlow_2024.R
@@ -24,8 +24,8 @@ sgpFlow.config <- list(
         year_lags.progression=as.integer(NULL),
         year_lags.projection.sequence=c(1L, 1L, 1L, 1L, 1L),
         max.order.for.progression=2L,
-#        growth.distributions=list("UNIFORM_RANDOM", "50", "BETA", "FREE_REDUCED_LUNCH_STATUS")),
-        growth.distributions=list("UNIFORM_RANDOM", "50")),
+#        growth.distributions=list("UNIFORM_RANDOM", "50", "1", "99", "BETA", "FREE_REDUCED_LUNCH_STATUS")),
+        growth.distributions=list("UNIFORM_RANDOM", "50", "1", "99")),
     ELA_GRADE_3 = list(
         grade.progression="3",
         grade.projection.sequence=c("4", "5", "6", "7", "8"),
@@ -34,13 +34,12 @@ sgpFlow.config <- list(
         year_lags.progression=as.integer(NULL),
         year_lags.projection.sequence=c(1L, 1L, 1L, 1L, 1L),
         max.order.for.progression=2L,
-#        growth.distributions=list("UNIFORM_RANDOM", "50", "BETA", "FREE_REDUCED_LUNCH_STATUS")),
-        growth.distributions=list("UNIFORM_RANDOM", "50"))
+#        growth.distributions=list("UNIFORM_RANDOM", "50", "1", "99", "BETA", "FREE_REDUCED_LUNCH_STATUS")),
+        growth.distributions=list("UNIFORM_RANDOM", "50", "1", "99"))
 )
 
 Indiana_sgpFlow <- sgpFlow(
                     sgp_object = Indiana_Data_LONG_2024,
                     sgpFlow.config = sgpFlow.config,
-                    projection.splineMatrices = projection.splineMatrices,
-                    parallel.config = NULL)
+                    projection.splineMatrices = projection.splineMatrices)
 
